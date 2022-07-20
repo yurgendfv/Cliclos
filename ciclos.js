2b7dcc3b370c30002ba17f7985d8c5645ea9e68f
yurgen <yurgendavidf@gmail.com>
2022-07-20 18:32:50 -0400

    ciclosEntrega

---
+++ ciclos.js
@@ -0,0 +1,91 @@
+/* Ciclo simple. */
+
+for (let i=21; i > 0; i--){
+    if(i=== 21){
+
+        alert("Resumen del Episodio 8 de la temporada 18 de South Park. Titulado ¨La Polla Mágica¨")
+
+    }
+
+    if(i === 20){
+
+        alert ("Acto" + " " + i + " " + "Kyle, Stan, Cartman y Kenny descubren, Magic The Gathering");
+
+    }; if (i === 19){
+
+        alert ("Acto" + " " + i + " " + "Kenny es bueno en Magic The Gathering, y vence a todos los nerds de la ciudad.")
+
+    }; if(i === 18){
+
+        alert ("Acto" + " " + i + " " + "Los chicos presumen en la escuela de lo violento que fue como Kenny polimorfio a su enemigo en una rana. Mitchel el bedel, los escucha y los invita a uan mierda mas violenta, les recomienda llevar bolsas para vomitar, y les dice la direccion.")
+
+    }; if(i === 17){
+
+        alert ("Acto" + " " + i + " " + "Los chicos van en la noche a la direccion, y se dan cuenta que es el city wok, el chinito dueño del boliche abre la puerta y dice que esta celado de noche, pero ellos dicen que mitchel el bedel los envio.")
+
+    }; if(i === 16){
+
+        alert("Acto" + " " + i + " " + "El chinito les pide 5 dolales para entrar por cada uno. Al entrar ven a dos pollas tirandose encatamientos, ellos nunca habían visto a dos pollas darse tan duro en sus vidas, eran unas grandes pollas.")
+
+    }; if(i === 15){
+
+        alert("Acto" + " " + i + " " + "Los chicos quedan extasiados con el nivel en que aquellas pollas se lanzaban hechizos a mansalva, pero Kenny se sentía mal porque no creía justo que obligaran a las pollas a destruirse en el juego de pollas magicas, creía si jugaban debía ser porque ellas asi lo querían.")
+
+    }; if(i === 14){
+
+        alert("Acto" + " " + i + " " + "Por Tanto Cartman dio la idea de comprar una polla para hacer sentir mejor a Kenny. Mientras tanto la Policia buscaba a los degenerados que hacian que estos pobres animales pelearan entre si lanzandose hechizos de congelamiento y demas, y claro al estilo mas racista y único de south park, estaban buscando entre los mas pobres.")
+    
+    }; if(i === 13){
+
+        alert("Acto" + " " + i + " " + "Por otra parte en la casa de Stan el papá de Stan se entero de que estaban adentrandose en el mundo de la magia de pollas por lo que decidio enseñarles como hacerlo con responsabilidad, asi que se saco su pene y comenzo a hacerlo mover libremente de un lado a otro de una manera impensable, a lo que los muchachos respondieron que eso no era magia de pollas tapandose los ojos.")
+    
+    }; if(i === 12){
+
+        alert("Acto" + " " + i + " " + "Acto seguido los chicos fueron al criador de Pollas para Magic The gathering local, para comprar su propia polla magica. Ellos entran y ven gran variedad de pollas mientras el dueño del criadero les dice las cualidades de cada polla, mientras una gastan mas mana otras son mas calculadoras, al final es Stan quien encuentra la polla perfecta, una pequeña polla la cual no saben que cartas usan.")
+    
+    }; if(i === 11){
+
+        alert("Acto" + " " + i + " " + "Teniendo la Polla faltaba el nombre, asi que deciden llamarla Macnnugets. Esa misma noche la llevan al city wok, y gana su primera batalla con una demostración soberbía. Por otra parte el papá de Stan obsesionado con su magia de pollas brinda un espectaculo de magia de pene, a unos nenes en una fiesta de cumpleaños, en donde su acto principal fue rebanarse el pene a la mitad para que despues apareciera en la parte de atras de la oreja de una niña. Obviamente los niños quedan traumados jajajaja.")
+
+    }; if(i === 10){
+
+        alert("Acto" + " " + i + " " + "Mientras tanto los chicos al salir del city wok con su polla victoriosa, un hombre se acerca y les dice que tienen una gran polla, que el nunca vio una polla luchar asi antes, ellos agradecen el cumplido. El hombre les ofrece llevar a la polla al siguiente nivel, y especifica que no sera una restaurant chino tan barato como ese del que salian que iba a ser una restaurant chino bien establecido. Algo no tan racista para ser sincero jajajaja. Ellos aceptan y se van.")
+
+    }; if(i === 9){
+
+        alert("Acto" + " " + i + " " + "Retomando al Padre de Stan, los padres de la niña de la cual el saco la mitad de su pene llamaron a la policia para reportar la magia de pollas, y el padre le comenta a la policia lo que había hecho con su pene en la fiesta de los niños, y al  parecer tambien le causo asombro al padre de la niña jajajaja. Pero el policia le pregunta que cartas estaban usando porque el esta tras los abusadores que obligan a las pollas a jugar en el juego violento de Magic The Gathering. Al final arrestan al padre de la niña por obstrucción a la justicia, típico en los sitemas de justicia de hoy en día.")
+
+    }; if(i === 8){
+        alert("Acto" + " " + i + " " + "Llega la noche de la pelea de McNugets en el otro nivel, esta vez llegan a Panda Xpress una franquicia china mas establecida. Al tocar el chinito dice que esta celado pero los chicos le dicen que vienen por la magia de pollas, este les pide 10 dolales y los deja pasar. Pero adentro estaba ocurriendo una masacre pues había una polla que era un total mounstro llamada Gadnuk: Breaker of Worlds. La cual no había perdido ni una sola vida en ninguna batalla.")
+
+    }; if(i === 7){
+
+        alert("Acto" + " " + i + " " + "McNugets al ver a su contrincante se acobarda y no quiere pelear, pero el organizador la quiere obligar, asi que Kenny impide esto y cambia de puesto con McNugets, para no hacerla pelear en contra de su voluntad, que grande Kenny. Todo se reanuda por ESPN illegal, cuya parrilla ofrece corrida de toros, peleas de perros y magia de pollas, en el sexto movimiento de Gadnuk, quien va ganando.")
+
+    }; if(i === 6){
+
+        alert("Acto" + " " + i + " " + "Gadnuk hace buenos movimientos pero Kenny es mas inteligente y arremete lanzando Sonda Sicogenica, que reduce 2 de vida a su oponente cada que este bareje el mazo, luego Gadnuk se defiende, pero kenny baja el legado de la vida y Gadnuk no supo que la golpeo, para luego lanzar fatalidad centellante y sacerdote guerrero uy luego empieza a lanzar cartas a mansalva, para el momento todo ya se torna demasiado violento y la multitud esta sorprendida por la violencia en las jugadas. El narrador de ESPN illegal pide que alguien lo pare. Y para suerte....")
+
+    }; if(i === 5){
+
+        alert("Acto" + " " + i + " " + "Y para suerte de Gadnuk llega la policia al sitio, si los mismos que han estado buscando a los degenerados que hacen pelear a las pollas en esa brutalidad jajajajaja, y detienen la masacre que estaba ocurriendo, exclaman que todos los presentes iran a la carcel y que sobretodo Kenny, quien debería tener verguenza por lo que estaba haciendo con esa pobre polla. Pero alguien se pregunta entre la multitud que como supieron que estaban allí.")
+
+    }; if (i === 4){
+
+        alert("Acto" + " " + i + " " + "A lo que responde el jefe de policia que se enteraron por los carteles que habían en toda la ciudad, y el organizador responde que ellos nunca usan carteles. Cuando de repente de fondo se escucha la voz de un presentador anunciando al ¨Al incomparable, Al hermoso, Randy el maravilloso¨, si el Papá de Stan que hace magía con el pene jajajaja, en su show de medio tiempo.")
+
+    }; if (i === 3){
+
+        alert("Acto" + " " + i + " " + "El cautiva la atención de los presentes con su entrada, y luego coloca una liga en su pene y lo mueve con la mente para luego hacerlo volar por el aire tal cual se tratara de un pequeño avión, luego sigue con su número suena la canción de Barbie girl, pero para poder hacer su acto final necesita un voluntario y el jefe de policia quien estaba fascinado con el show se ofrece, mientras los demas se estan yendo uno por uno del sotano de la franquicia china mas establecida.")
+    
+    }; if(i === 2){
+
+        alert("Acto" + " " + i + " " + "Y así pues el último acto consitia en vertir nitrogeno liquido en el pene de Randy, no antes de advertir que la magia de pollas debe ser siempre realizada por profesionales, así que acto seguido el jefe de policias toma el nitrogeno y con un embudo lo vierte en el pene de Randy, por razones obvias, el pene de este se congela, y saca un arma de fuego se dispara en el pene este se fragmenta en muchos pedazos, pero gracias a la maravillosa mágia de polla, el pene de Randy sale de la boca del jefe de policia, y como si nada pasara lo coloca de nuevo en sus pantalones.")
+
+    }; if(i === 1){
+        
+        alert("Acto" + " " + i + " " + "Al siguiente día los chicos de nuevo estan en la primaria de South Park contandoles a todos lo brutal y masculina que fue la noche anterior, con Kenny destruyendo a Gadnuk y  el papá de Stan volandose el pene en muchos pedazos jajajajaja. Solo me queda escribir no olviden dejarle la mágia de pollas a los profesionales.")
+
+    }
+
+}
